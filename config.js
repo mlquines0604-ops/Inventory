e1467ec4cc11ba8dd6de85641b9fcbf1b10d32f6
mlquines0604-ops <mlquines0604@gmail.com>
2026-03-13 16:33:34 +0800

    Add configuration file with API and client details

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG={
+API_URL:"https://script.google.com/macros/s/AKfycbzTSeMMHuhvddUcEBmHNCA3HBkuzvZOHfR6wqFRVcrdiy0Q85U6FRjY9RO2reBHH-2wzw/exec",
+GOOGLE_CLIENT_ID:"955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
+COMPANY_DOMAIN:"gmail.com"
+}
